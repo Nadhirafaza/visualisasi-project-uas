48e7e297e17282b54a084860ffb0d6a95911611b
Nadhirafaza <nadhirafazayy@gmail.com>
2023-07-07 16:28:10 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/grafik1', [DashboardController::class, 'grafik1']);
+Route::get('/grafik2', [DashboardController::class, 'grafik2']);
+Route::get('/grafik3', [DashboardController::class, 'grafik3']);
+Route::get('/grafik4', [DashboardController::class, 'grafik4']);
+Route::get('/grafik5', [DashboardController::class, 'grafik5']);
+Route::get('/grafik6', [DashboardController::class, 'grafik6']);
+
+
